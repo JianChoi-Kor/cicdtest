349f437e6c45ea5d1ea569c6d6ccfcfde91eb2b1
JianChoi-Kor <wldks79@gmail.com>
2024-12-28 19:29:14 +0900

    cicd

---
+++ src/main/java/com/example/cicdtest/controller/CicdController.java
@@ -0,0 +1,24 @@
+package com.example.cicdtest.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cicd")
+public class CicdController {
+
+    @Value("${app.test.value}")
+    private String testValue;
+
+    @GetMapping("/test")
+    public String cicdTest() {
+        return "cicd 'version 1.0.0'";
+    }
+
+    @GetMapping("/test1")
+    public String cicdTest1() {
+        return testValue;
+    }
+}

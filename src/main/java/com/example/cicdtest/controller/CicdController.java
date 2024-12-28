0943e2a14643ae9ce627d42f90588cab72851327
JianChoi-Kor <wldks79@gmail.com>
2024-12-28 22:55:35 +0900

    cicd

--- src/main/java/com/example/cicdtest/controller/CicdController.java
+++ src/main/java/com/example/cicdtest/controller/CicdController.java
@@ -1,11 +1,13 @@
 package com.example.cicdtest.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/cicd")
 public class CicdController {
 
